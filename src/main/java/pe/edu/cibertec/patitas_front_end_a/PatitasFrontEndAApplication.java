5caa42d48f1799486b7f54b9a285b2f6ad2772c5
NicolasSembrera <nsembrera@gmail.com>
2024-09-17 20:43:55 -0500

    Carga Inicial de Patitas

---
+++ src/main/java/pe/edu/cibertec/patitas_front_end_a/PatitasFrontEndAApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_front_end_a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatitasFrontEndAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatitasFrontEndAApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/patitas_front_end_a/PatitasFrontEndAApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.patitas_front_end_a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatitasFrontEndAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
